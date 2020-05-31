74054ec2482d1288af8ffcd97d7d511e0879c44c
thanawat d <thanawat.d@lqid360.com>
2020-05-31 14:55:49 +0700

    add functionality view and submit link

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -1,38 +1,26 @@
 package com.vega.springit;
 
-import com.vega.springit.Repository.CommentRepository;
-import com.vega.springit.Repository.LinkRepository;
-import com.vega.springit.domain.Comment;
-import com.vega.springit.domain.Link;
-import org.springframework.boot.CommandLineRunner;
+import org.ocpsoft.prettytime.PrettyTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringitApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
 	}
 
-//	@Bean
-	CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository) {
-		return args -> {
-			Link link = new Link("Getting Started with Spring Boot 2", "https://therealdavega.com/spring-boot-2");
-			linkRepository.save(link);
-
-			Comment comment = new Comment("This Spring Boot 2 link is awesome!", link);
-			commentRepository.save(comment);
-			link.addComment(comment);
-
-			System.out.println("We just inserted a link into and comment");
-			System.out.println("=================================================");
-
-//			Link firstLink = linkRepository.findByTitle("Getting Started with Spring Boot 2");
-//			System.out.println(firstLink.getTitle());
-
-		};
+	@Bean
+	PrettyTime prettyTime() {
+		return new PrettyTime();
 	}
 
 }
--- src/main/java/com/vega/springit/controller/LinkController.java
+++ src/main/java/com/vega/springit/controller/LinkController.java
@@ -2,48 +2,70 @@ package com.vega.springit.controller;
 
 import com.vega.springit.Repository.LinkRepository;
 import com.vega.springit.domain.Link;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
+import javax.validation.Valid;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/links")
+@Controller
 public class LinkController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LinkController.class);
 
     private LinkRepository linkRepository;
 
-    public LinkController(LinkRepository linkRepository) {
+    private LinkController(LinkRepository linkRepository) {
         this.linkRepository = linkRepository;
     }
 
-    // list
     @GetMapping("/")
-    public List<Link> list() {
-        return linkRepository.findAll();
+    public String list(Model model) {
+        model.addAttribute("links", linkRepository.findAll());
+        return "link/list";
     }
 
-    // CRUD
-    @PostMapping("/create")
-    public Link create(@ModelAttribute Link link) {
-        return linkRepository.save(link);
+    @GetMapping("/link/{id}")
+    public String read(@PathVariable Long id, Model model) {
+        Optional<Link> link = linkRepository.findById(id);
+        if( link.isPresent() ) {
+            model.addAttribute("link",link.get());
+//            ​model.addAttribute("success", model.containsAttribute("success"));
+            return "link/view";
+        } else {
+            return "redirect:/";
+        }
     }
 
-    // /links/1
-    @GetMapping("/{id}")
-    public Optional<Link> read(@PathVariable Long id) {
-        return linkRepository.findById(id);
+    @GetMapping("/link/submit")
+    public String newLinkForm(Model model) {
+        model.addAttribute("link",new Link());
+        return "link/submit";
     }
 
-    @PutMapping("/{id}")
-    public Link update(@PathVariable Long id, @ModelAttribute Link link) {
-        // get the link id
-        return linkRepository.save(link);
+    @PostMapping("/link/submit")
+    public String createLink(@Valid Link link, BindingResult bindingResult, Model model
+            , RedirectAttributes redirectAttributes) {
+        if(bindingResult.hasErrors()) {
+            logger.info("Validation errors were found while submitting a new link.");
+            model.addAttribute("link",link);
+            return "link/submit";
+        } else {
+            // save our link
+            linkRepository.save(link);
+            logger.info("New Link was saved successfully.");
+            redirectAttributes
+                    .addAttribute("id", link.getId())
+                    .addFlashAttribute("success",true);
+            return "redirect:/link/{id}";
+        }
     }
 
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-        linkRepository.deleteById(id);
-    }
 }
