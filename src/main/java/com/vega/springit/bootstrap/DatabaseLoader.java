72b89cdcb293754fe0496b2052f1c7ea0cfee2cd
thanawat d <thanawat.d@lqid360.com>
2020-06-21 19:53:20 +0700

    Add security endpoint

--- src/main/java/com/vega/springit/bootstrap/DatabaseLoader.java
+++ src/main/java/com/vega/springit/bootstrap/DatabaseLoader.java
@@ -2,17 +2,35 @@ package com.vega.springit.bootstrap;
 
 import com.vega.springit.Repository.CommentRepository;
 import com.vega.springit.Repository.LinkRepository;
+import com.vega.springit.Repository.RoleRepository;
+import com.vega.springit.Repository.UserRepository;
 import com.vega.springit.domain.Link;
+import com.vega.springit.domain.Role;
+import com.vega.springit.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 @Component
 public class DatabaseLoader implements CommandLineRunner {
     private LinkRepository linkRepository;
     private CommentRepository commentRepository;
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+
+    @Autowired
+    public DatabaseLoader(LinkRepository linkRepository, CommentRepository commentRepository, UserRepository userRepository, RoleRepository roleRepository) {
+        this.linkRepository = linkRepository;
+        this.commentRepository = commentRepository;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
 
     public DatabaseLoader(LinkRepository linkRepository, CommentRepository commentRepository) {
         this.linkRepository = linkRepository;
@@ -21,6 +39,10 @@ public class DatabaseLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        
+        // add users and roles
+        addUserAndRoles();
+        
         Map<String,String> links = new HashMap<>();
         links.put("Securing Spring Boot APIs and SPAs with OAuth 2.0","https://auth0.com/blog/securing-spring-boot-apis-and-spas-with-oauth2/?utm_source=reddit&utm_medium=sc&utm_campaign=springboot_spa_securing");
         links.put("Easy way to detect Device in Java Web Application using Spring Mobile - Source code to download from GitHub","https://www.opencodez.com/java/device-detection-using-spring-mobile.htm");
@@ -41,5 +63,28 @@ public class DatabaseLoader implements CommandLineRunner {
 
         long linkCount = linkRepository.count();
         System.out.println("Number of links in the database: " + linkCount );
+    }
+
+    private void addUserAndRoles() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String secret = "{bcrypt}" + encoder.encode("password");
+
+        Role userRole = new Role("ROLE_USER");
+        roleRepository.save(userRole);
+        Role adminRole = new Role("ROLE_ADMIN");
+        roleRepository.save(adminRole);
+
+        User user = new User("user@gmail.com",secret,true);
+        user.addRole(userRole);
+        userRepository.save(user);
+
+        User admin = new User("admin@gmail.com",secret,true);
+        admin.addRole(adminRole);
+        userRepository.save(admin);
+
+        User master = new User("master@gmail.com",secret,true);
+        master.addRoles(new HashSet<>(Arrays.asList(userRole,adminRole)));
+        userRepository.save(master);
+
     }
 }
