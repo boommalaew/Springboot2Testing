10dc9933927018800ba609048676ab04f7530c09
thanawat d <thanawat.d@lqid360.com>
2020-10-08 22:20:14 +0700

    The ability to add new comment

--- src/main/java/com/vega/springit/domain/Comment.java
+++ src/main/java/com/vega/springit/domain/Comment.java
@@ -1,31 +1,56 @@
 package com.vega.springit.domain;
 
+import com.vega.springit.service.BeanUtil;
 import lombok.*;
+import org.ocpsoft.prettytime.PrettyTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 @Entity
 @Getter @Setter
-@ToString
 @Data
-@NoArgsConstructor
 public class Comment extends Auditable {
 
     @Id
     @GeneratedValue
     private Long id;
+
+    @NonNull
     private String body;
 
     // link
     @ManyToOne
+    @NonNull
     private Link link;
+
+    public Comment() {
+    }
 
     public Comment(@NonNull String body, @NonNull Link link) {
         this.body = body;
         this.link = link;
     }
 
+    public String getPrettyTime() {
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public void setLink(Link link) {
+        this.link = link;
+    }
+
+    public Link getLink() {
+        return link;
+    }
 }
